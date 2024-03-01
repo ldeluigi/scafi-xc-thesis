a2105ea8cd4691d12677fd0fd3ff34eb71457c51
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-03-01 16:54:54 +0100

    feat: engine design section

---
+++ listings/exchange-syntax.scala
@@ -0,0 +1,13 @@
+def exchange[T](initial: AggregateValue[T])(f: AggregateValue[T] => RetSend[AggregateValue[T]]): AggregateValue[T]
+
+import RetSend.{ *, given } // necessary to enable some of the styles below
+
+// To send and return the same value:
+exchange(0)(value => f(value))
+exchange(0)(value => retsend(f(value)))
+
+// To send and return potentially different values:
+exchange(0)(value => (f(value), f2(value)))
+exchange(0)(value => ret (f(value)) send f2(value))
+exchange(0)(value => ret(f(value)).send(f2(value)))
+exchange(0)(value => RetSend(f(value), f2(value)))
